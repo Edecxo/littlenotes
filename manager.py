a42e5e2a7f254ce1f28f1c4d89ea72804935b792
Edecxo <hunterlogan.eden@gmail.com>
2025-12-09 18:52:57 -0600

    added note formatting function, and tweaked storage functions

--- manager.py
+++ manager.py
@@ -1,3 +1,6 @@
+import storage
+import datetime
+
 '''
 Functions for converting data into json format as specified below.
 
@@ -18,9 +21,24 @@ Note json format
 '''
 
 
-def note_format(title, body, timestamp):
+def note_format(title, body):
     '''
     Formats the note into json.
     Refer to docstring at the beginning of this file.
     '''
-    pass
+    new_id = str(1)
+    existing_ids = [note['id'] for note in storage.note_list()]
+    while new_id in existing_ids:
+        new_id = str(int(new_id) + 1)
+
+    now = datetime.datetime.now()
+    new_note = {
+            'id': new_id,
+            'title': title,
+            'body': body,
+            'timestamp': {
+                'created': now,
+                'updated': now
+            }
+    }
+    return new_note
--- storage.py
+++ storage.py
@@ -2,32 +2,31 @@ import json
 
 """Functions for storing json objects into file."""
 
-def note_store(note, path):
+default_path = 'data/test.json'
+
+def note_store(note, path=default_path):
     """Adds a new note object at path."""
     with open('data/test.json', 'w'):
         pass
 
-def note_list(path):
+def note_list(path=default_path):
     """Lists all note objects at path."""
     with open('data/test.json', 'r') as file:
         data = json.loads(file.read())
+    all_notes = []
     for entry in data:
-        return (f'Note ID: {entry["id"]}\n' +
-                f'Title: {entry["title"]}\n'
-        )
+        all_notes.append(entry)
+    return all_notes
 
-def note_view(note_name, path):
+def note_retrieve(note_name, path=default_path):
     """Shows a specific note at path."""
     with open('data/test.json', 'r') as file:
         data = json.loads(file.read())
     for entry in data:
         if note_name == entry['id'] or note_name == entry['title']:
-            return (f'Note ID: {entry["id"]}\n' +
-                    f'Title: {entry["title"]}\n' +
-                    f'Body: {entry["body"]}\n'
-            )
+            return entry
     return f'"{note_name}" not found.'
 
-def note_delete(note_name, path):
+def note_delete(note_name, path=default_path):
     """Deletes a specific note at path."""
     pass
